973e29daca706ad3e92cbeae85b0d8b8517c3a58
wambetsa <wambetsae@gmail.com>
2023-09-13 15:42:56 -0700

    Write a function that adds two integers and returns the result.
    Prototype: int add(int, int);

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Check Holberton
+ * @x: An integer x
+ * @y: An integer y
+ * Description: Function that adds two integers and returns the result
+ * Return: Result to add x and x
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
