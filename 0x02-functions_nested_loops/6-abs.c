78894480b5fdf4a2c66be0a3f84a97566b9d404d
wambetsa <wambetsae@gmail.com>
2023-09-13 15:06:45 -0700

    Write a function that computes the absolute value of an integer.
    Prototype: int _abs(int);
    FYI: The standard library provides a similar function: abs. Run man abs to learn more.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Check description
+ * @r: An integer input or argument
+ * Description: The function returns absolute values of numbers
+ * Return: Absolut value of number i
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (i * -1);
+}
