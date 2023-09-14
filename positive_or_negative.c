bfcc82aa71ad708f28a8b7e88dd3a78868d57851
wambetsa <wambetsae@gmail.com>
2023-09-14 13:00:35 -0700

    test positive negative or zero

--- positive_or_negative.c
+++ positive_or_negative.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	if (n > 0)
-	{
-		printf("%d" n " is positive");
-	}
+		printf("%d is positive\n", n);
 	else if (n == 0)
-	{
-		printf("%d" n " is zero");
-	}
+		printf("%d is zero\n", n);
 	else
-	{
-		printf("%d" n " is negative");
-	}
+		printf("%d is negative\n", n);
 	return (0);
 }
