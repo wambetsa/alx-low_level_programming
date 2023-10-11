6872179aff0d2b683590b9f1658b31b33b36f6a1
wambetsa <wambetsae@gmail.com>
2023-10-11 02:30:26 -0700

    typedef assignment

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - initialize a variable of type struct dog.
+ * @d: pointer to the adress to initialize.
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
