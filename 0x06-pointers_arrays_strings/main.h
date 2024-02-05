2db692e8909bd78aa50b1cb37905ab5e4887e0c6
wambetsa emmanuel <wambetsae@gmail.com>
2024-02-05 18:13:35 +0300

    infinite add

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 char *_strstr(char *haystack, char *needle);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 #endif
