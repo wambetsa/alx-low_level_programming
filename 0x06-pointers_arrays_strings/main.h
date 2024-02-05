875a4dda5b9e8380e38433c6c3f707ea37feee78
wambetsa emmanuel <wambetsae@gmail.com>
2024-02-05 18:19:57 +0300

    print buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ char *leet(char *);
 char *rot13(char *);
 char *_strstr(char *haystack, char *needle);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 void print_number(int n);
 #endif
